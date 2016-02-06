e85e617b323bf82ef137f6540e3c3151f71b7514
Jonas Rauber <git@jonasrauber.de>
2016-02-06 12:40:03 +0100

    made it more similar to the ipython Dockerfile: https://github.com/ipython/docker-notebook/blob/master/scipystack/build_scipy_stack.sh

--- cuda7.0-cudnn4/build_scipy_stack.sh
+++ cuda7.0-cudnn4/build_scipy_stack.sh
@@ -12,27 +12,24 @@ cp /tmp/numpy-site.cfg numpy/site.cfg
 git clone -q https://github.com/scipy/scipy.git
 cp /tmp/scipy-site.cfg scipy/site.cfg
 
-apt-get update
+apt-get -y update
+apt-get -y install git-core build-essential gfortran python3-dev curl
 apt-get build-dep -y python3 python3-numpy python3-scipy python3-matplotlib cython3 python3-h5py
+apt-get install -y build-essential python3-dev
 
 curl https://bootstrap.pypa.io/get-pip.py | python2
 curl https://bootstrap.pypa.io/get-pip.py | python3
 
-apt-get -y remove cython
-# apt-get -y remove numpy
-
-for v in 2 3
-do
-  PYTHON="python$v"
-  PIP="pip$v"
-
-  $PIP install --upgrade cython
+for PYTHONVER in 2 3 ; do
+  PYTHON="python$PYTHONVER"
+  PIP="pip$PYTHONVER"
 
   # Build NumPy and SciPy from source against OpenBLAS installed
-  (cd numpy && $PIP install .)
-  (cd scipy && $PIP install .)
-
+  (cd numpy && $PYTHON setup.py install)
+  (cd scipy && $PYTHON setup.py install)
+  
   # The rest of the SciPy Stack
+  $PIP install cython
   $PIP install pandas scikit-learn
   $PIP install matplotlib
   $PIP install seaborn
@@ -45,12 +42,15 @@ do
   $PIP install git+https://github.com/Theano/Theano.git
   $PIP install git+https://github.com/Lasagne/Lasagne.git
   $PIP install bokeh
-
-  # Install additional developer tools
   $PIP install mock
   $PIP install pytest
-  $PIP install
 done
+
+# Reduce the image size
+apt-get autoremove -y
+apt-get clean -y
+
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 cd /
 rm -rf /tmp/build
