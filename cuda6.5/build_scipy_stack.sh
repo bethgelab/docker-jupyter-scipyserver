1d535efdd2af023b1be81c23be3ba43c1e487717
Jonas Rauber <git@jonasrauber.de>
2016-02-07 09:51:56 +0100

    trying to split scipy/numpy from the other python packages

--- cuda6.5/build_scipy_stack.sh
+++ cuda6.5/build_scipy_stack.sh
@@ -13,9 +13,10 @@ git clone -q https://github.com/scipy/scipy.git
 cp /tmp/scipy-site.cfg scipy/site.cfg
 
 apt-get -y update
-apt-get -y install git-core build-essential gfortran python3-dev curl
-apt-get build-dep -y python3 python3-numpy python3-scipy python3-matplotlib cython3 python3-h5py
-apt-get install -y build-essential python3-dev
+## apt-get -y install git-core build-essential gfortran python3-dev curl
+## apt-get build-dep -y python3 python3-numpy python3-scipy python3-matplotlib cython3 python3-h5py
+apt-get build-dep -y python3 python3-numpy python3-scipy cython3
+## apt-get install -y build-essential python3-dev
 
 curl https://bootstrap.pypa.io/get-pip.py | python2
 curl https://bootstrap.pypa.io/get-pip.py | python3
@@ -33,20 +34,20 @@ for PYTHONVER in 2 3 ; do
   (cd scipy && $PYTHON setup.py install)
   
   # The rest of the SciPy Stack
-  $PIP install pandas scikit-learn
-  $PIP install matplotlib
-  $PIP install seaborn
-  $PIP install h5py
-  $PIP install yt
-  $PIP install sympy
-  $PIP install patsy
-  $PIP install ggplot
-  $PIP install statsmodels
-  $PIP install git+https://github.com/Theano/Theano.git
-  $PIP install git+https://github.com/Lasagne/Lasagne.git
-  $PIP install bokeh
-  $PIP install mock
-  $PIP install pytest
+##  $PIP install pandas scikit-learn
+##  $PIP install matplotlib
+##  $PIP install seaborn
+##  $PIP install h5py
+##  $PIP install yt
+##  $PIP install sympy
+##  $PIP install patsy
+##  $PIP install ggplot
+##  $PIP install statsmodels
+##  $PIP install git+https://github.com/Theano/Theano.git
+##  $PIP install git+https://github.com/Lasagne/Lasagne.git
+##  $PIP install bokeh
+##  $PIP install mock
+##  $PIP install pytest
 done
 
 # Reduce the image size
